1ebb0af67fea7c5317755632d877960300bbdf93
Alex Pogue <apogue@us.ibm.com>
2018-10-03 13:41:40 -0500

    Fix bug 3

--- newrelic_api/notification_channel_policies.py
+++ newrelic_api/notification_channel_policies.py
@@ -29,7 +29,7 @@ class NotificationChannelPolicies(Resource):
 
         params = [
             'policy_id={}'.format(policy_id),
-            'channel_ids={}'.format(','.join(channel_ids))
+            'channel_ids={}'.format(','.join(str(x) for x in channel_ids))
         ]
 
         return self._put(
